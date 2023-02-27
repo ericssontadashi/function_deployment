6ba3ce69eb9a532c70d2754464705b21931d004d
Ericsson Oliveira <cssonddos@gmail.com>
2023-02-27 16:30:19 -0300

    Working pipeline

---
+++ README.md
@@ -0,0 +1,3 @@
+![Job execution](images/1.png)
+
+![Function Deployment](images/2.png)
---
+++ SampleFunctionApp.Test/FunctionsTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+
+namespace SampleFunctionApp.Test
+{
+    public class Function1Test
+    {
+        private readonly ILogger logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+        [Fact]
+        public async void HttpTriggerWithParams()
+        {
+            var request = TestFactory.CreateHttpRequest("name", "Bill");
+            var response = (OkObjectResult)await Function1.Run(request, logger);
+            Assert.Equal("Hello, Bill. This HTTP triggered function executed successfully.", response.Value);
+        }
+
+        [Fact]
+        public async void HttpTriggerWithoutParams()
+        {
+            var request = TestFactory.CreateHttpRequest("", "");
+            var response = (OkObjectResult)await Function1.Run(request, logger);
+            Assert.Equal("This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.", response.Value);
+        }
+    }
+}
